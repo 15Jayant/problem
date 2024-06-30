e9a90de6ddd9ab9480c9a49e0fdf8d9a53ed0de0
Jayant kumar <139684213+15Jayant@users.noreply.github.com>
2024-06-30 08:58:06 +0530

    Add files via upload

---
+++ ABCstring.java
@@ -0,0 +1,16 @@
+public class ABCstring
+{   public static void main(String[] args) {
+    
+
+    String s= "i am Jayant";
+    String s1="";
+   // char []c = s.toCharArray();
+    for(int i=0;i<s.length();i++)
+    {
+            if(s.charAt(i)==' ') 
+            {
+                 
+            }
+    }
+}
+}
